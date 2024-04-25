68acea62d8c6d9949d042c43fab3aaac8bc4ad81
Anand Bose <anandbose16@gmail.com>
2024-04-25 23:08:55 +0530

    Add Google Play deployment workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.anandbose.blogapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 10
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
